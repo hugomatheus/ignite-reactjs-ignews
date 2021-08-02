9c4064b076e29a465f6882b198258453075df97d
Hugo <hugo.azevedo@al.senai.br>
2021-08-02 20:12:14 -0300

    component SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from './home.module.scss';
 import imgAvatar from '../../../ignews/public/images/avatar.svg'; 
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
               Get access to all the publications <br/>
               <span>for $9.90 month</span>
             </p>
+            <SubscribeButton />
           </section>
           <Image src={imgAvatar} alt=""  />
         </main>
