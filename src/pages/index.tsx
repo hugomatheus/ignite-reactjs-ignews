ca8ee8fbee019b1640b3e41b58a358d43f295f86
Hugo <hugo.azevedo@al.senai.br>
2021-08-02 19:49:47 -0300

    page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
+import imgAvatar from '../../../ignews/public/images/avatar.svg'; 
 
 export default function Home() {
   return (
       <>
         <Head>
-          <title>Início | ig.news</title>
+          <title>Home | ig.news</title>
         </Head>
-        <h1> hello word</h1>
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publications <br/>
+              <span>for $9.90 month</span>
+            </p>
+          </section>
+          <Image src={imgAvatar} alt=""  />
+        </main>
       </>
     )
 }
