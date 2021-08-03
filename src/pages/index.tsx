6dc6b98cb8a948ee682277b5e08e841e2fc2db1f
Hugo <hugo.azevedo@al.senai.br>
2021-08-03 18:14:18 -0300

    use Api Stripe with SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,17 @@ import Image from 'next/image';
 import styles from './home.module.scss';
 import imgAvatar from '../../../ignews/public/images/avatar.svg'; 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { GetServerSideProps } from 'next';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface IHomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: IHomeProps) {
   return (
       <>
         <Head>
@@ -16,12 +25,34 @@ export default function Home() {
             <h1>News about the <span>React</span> world.</h1>
             <p>
               Get access to all the publications <br/>
-              <span>for $9.90 month</span>
+              <span>for {product.amount} month</span>
             </p>
-            <SubscribeButton />
+            <SubscribeButton priceId={product.priceId} />
           </section>
           <Image src={imgAvatar} alt=""  />
         </main>
       </>
     )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JKAwSJMCVEAdLlr8RHERzRA', {
+    expand: ['product']
+  });
+
+  const priceAmount = price.unit_amount || 0;
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(priceAmount / 100),
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
