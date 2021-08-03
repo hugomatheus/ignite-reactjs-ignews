b8b79164056b0b07088e7942d31a9bcef3d038ef
Hugo <hugo.azevedo@al.senai.br>
2021-08-03 18:47:02 -0300

    change get price product stripe using Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from './home.module.scss';
 import imgAvatar from '../../../ignews/public/images/avatar.svg'; 
 import { SubscribeButton } from '../components/SubscribeButton';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 
 interface IHomeProps {
@@ -36,7 +36,7 @@ export default function Home({product}: IHomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JKAwSJMCVEAdLlr8RHERzRA', {
     expand: ['product']
   });
@@ -53,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24  // 24 hours
   }
 }
