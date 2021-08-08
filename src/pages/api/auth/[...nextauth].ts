d2972fbb51ee517440cf9daf28476845d95afdb7
Hugo <hugo.azevedo@al.senai.br>
2021-08-08 12:38:55 -0300

    save user in faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { faunadb } from '../../../services/faunadb';
+import { query as q } from 'faunadb';
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,31 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        await faunadb.query(
+          q.Create(
+            q.Collection('users'),
+            {data: {email: user.email}}
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    // async redirect(url, baseUrl) {
+    //   return baseUrl
+    // },
+    // async session(session, user) {
+    //   return session
+    // },
+    // async jwt(token, user, account, profile, isNewUser) {
+    //   return token
+    // }
+  }
 })
