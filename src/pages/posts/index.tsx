f61956b33202597ef5adc0e99730c622fe708f7d
Hugo <hugo.azevedo@al.senai.br>
2021-08-10 21:24:06 -0300

    list posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,25 @@
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import Head from 'next/head';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  id: string;
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+ posts: Post[];
+}
+
+export default function Posts({posts}: PostsProps) {
   return (
     <>
       <Head>
@@ -14,30 +27,15 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet tempor dolor. Praesent condimentum mauris eu diam egestas tristique. Etiam ornare efficitur velit, ac malesuada lorem fringilla aliquet. Duis commodo odio vitae lacus porta maximus eget sit amet ligula. Maecenas in sapien nibh. Proin finibus velit ac est sodales maximus non rutrum dolor. In tempus purus sed mauris consequat maximus. Praesent ac augue ante. Quisque efficitur rutrum gravida. Suspendisse ut tellus nunc. Quisque quis odio in augue congue semper. Maecenas et enim sapien. Morbi eu lacinia risus. Nulla egestas diam ut tellus tristique, a accumsan lorem posuere. Suspendisse maximus nisl eu tellus laoreet, ut gravida tortor pharetra.
-              Suspendisse a ex orci. Aliquam efficitur odio erat, vitae iaculis justo pulvinar ac. Donec feugiat, arcu id tempor dapibus, quam elit hendrerit purus, nec efficitur nisi nisl nec lorem. Sed interdum maximus est, non feugiat libero laoreet vitae. Vivamus sit amet vulputate nisi. Nam purus tortor, hendrerit non condimentum sed, iaculis ac est. Sed viverra mauris porttitor dolor imperdiet tincidunt vel sit amet tortor. Quisque porta neque sed augue rhoncus, nec interdum diam posuere. Sed sed lacinia lacus, nec vestibulum justo. Aenean euismod nunc lectus, ac finibus felis sollicitudin eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel ante sapien.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet tempor dolor. Praesent condimentum mauris eu diam egestas tristique. Etiam ornare efficitur velit, ac malesuada lorem fringilla aliquet. Duis commodo odio vitae lacus porta maximus eget sit amet ligula. Maecenas in sapien nibh. Proin finibus velit ac est sodales maximus non rutrum dolor. In tempus purus sed mauris consequat maximus. Praesent ac augue ante. Quisque efficitur rutrum gravida. Suspendisse ut tellus nunc. Quisque quis odio in augue congue semper. Maecenas et enim sapien. Morbi eu lacinia risus. Nulla egestas diam ut tellus tristique, a accumsan lorem posuere. Suspendisse maximus nisl eu tellus laoreet, ut gravida tortor pharetra.
-              Suspendisse a ex orci. Aliquam efficitur odio erat, vitae iaculis justo pulvinar ac. Donec feugiat, arcu id tempor dapibus, quam elit hendrerit purus, nec efficitur nisi nisl nec lorem. Sed interdum maximus est, non feugiat libero laoreet vitae. Vivamus sit amet vulputate nisi. Nam purus tortor, hendrerit non condimentum sed, iaculis ac est. Sed viverra mauris porttitor dolor imperdiet tincidunt vel sit amet tortor. Quisque porta neque sed augue rhoncus, nec interdum diam posuere. Sed sed lacinia lacus, nec vestibulum justo. Aenean euismod nunc lectus, ac finibus felis sollicitudin eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel ante sapien.
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet tempor dolor. Praesent condimentum mauris eu diam egestas tristique. Etiam ornare efficitur velit, ac malesuada lorem fringilla aliquet. Duis commodo odio vitae lacus porta maximus eget sit amet ligula. Maecenas in sapien nibh. Proin finibus velit ac est sodales maximus non rutrum dolor. In tempus purus sed mauris consequat maximus. Praesent ac augue ante. Quisque efficitur rutrum gravida. Suspendisse ut tellus nunc. Quisque quis odio in augue congue semper. Maecenas et enim sapien. Morbi eu lacinia risus. Nulla egestas diam ut tellus tristique, a accumsan lorem posuere. Suspendisse maximus nisl eu tellus laoreet, ut gravida tortor pharetra.
-              Suspendisse a ex orci. Aliquam efficitur odio erat, vitae iaculis justo pulvinar ac. Donec feugiat, arcu id tempor dapibus, quam elit hendrerit purus, nec efficitur nisi nisl nec lorem. Sed interdum maximus est, non feugiat libero laoreet vitae. Vivamus sit amet vulputate nisi. Nam purus tortor, hendrerit non condimentum sed, iaculis ac est. Sed viverra mauris porttitor dolor imperdiet tincidunt vel sit amet tortor. Quisque porta neque sed augue rhoncus, nec interdum diam posuere. Sed sed lacinia lacus, nec vestibulum justo. Aenean euismod nunc lectus, ac finibus felis sollicitudin eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer vel ante sapien.
-            </p>
-          </a>
+          {posts.map(post => (
+            <a key={post.id} href="#">
+             <time>{post.updatedAt}</time>
+             <strong>{post.title}</strong>
+             <p>
+              {post.excerpt}
+             </p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -53,7 +51,21 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100
   });
   // console.log(JSON.stringify(response, null, 2));
+
+  const posts = response.results.map(post => {
+    return {
+      id: post.id,
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find((content: { type: string; }) => content.type === 'paragraph')?.text ?? '',
+      updatedAt: post.last_publication_date ? new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }) : ''
+    };
+  });
   return { 
-    props: {}
+    props: {posts}
   }
 }
