d71a1e054b05c06d82d027870c9be053f6adeffb
Hugo <hugo.azevedo@al.senai.br>
2021-08-11 20:43:50 -0300

    navigation and active link (menu)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image';
 import logo from '../../../../ignews/public/images/logo.svg';
+import ActiveLink from './ActiveLink';
 import { SignInButton } from './SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src={logo} alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+             <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
       <SignInButton />
       </div>
